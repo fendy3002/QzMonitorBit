987a5fcfca9a856de7f69f9d7134754bad7740ff
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2017-07-19 12:05:51 +0700

    ignore certificate at all

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ import path from 'path';
 import dispatcher from './dispatcher';
 import context from './context.js';
 
+// ignore tls verify certificate
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 var io = socket(appConfig.port);
 context.io = io;
 context.appConfig = appConfig;
